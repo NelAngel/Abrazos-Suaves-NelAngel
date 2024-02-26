f4cd591f173bb615775c306bcbce40745c89dcde
Angel Chuchullo Chacca <achuchullo@unsa.edu.pe>
2024-02-26 10:01:10 -0500

    Se le arreglo el but q este tenia debido a que no cumplia su funcionamiento como debe ser

--- script.js
+++ script.js
@@ -1,27 +1,39 @@
+// Guardamos el estado del interruptor antes de que los botones desaparezcan
+var lastModeState = null;
+var modoToggle = document.getElementById('modo-toggle');
+if (modoToggle) {
+    lastModeState = modoToggle.checked;
+}
+
 window.addEventListener('scroll', function() {
     var toggleContainer = document.getElementById('toggle-container');
     var modoToggle = document.getElementById('modo-toggle');
     
     if (window.scrollY > 100) {
         toggleContainer.style.opacity = 0;
+        // Si el interruptor de oscurecimiento y aclarado estaba activado, lo desactivamos visualmente
+        if (modoToggle && modoToggle.checked) {
+            lastModeState = true;
+            modoToggle.checked = false;
+        }
     } else {
         toggleContainer.style.opacity = 1;
-    }
-    
-    // Asegurémonos de que el estado de la palanca se actualice cuando se desplace la página
-    if (modoToggle.checked && window.scrollY > 100) {
-        modoToggle.checked = false;
+        // Si el interruptor de oscurecimiento y aclarado estaba activado antes de desplazarse hacia abajo, lo activamos visualmente
+        if (modoToggle && lastModeState === true) {
+            modoToggle.checked = true;
+        }
     }
 });
 
 // Manejamos el evento change del checkbox de modo oscuro y claro
-var modoToggle = document.getElementById('modo-toggle');
-modoToggle.addEventListener('change', function() {
-    if (modoToggle.checked) {
-        // Activar el modo oscuro
-        document.body.classList.add('dark-mode');
-    } else {
-        // Desactivar el modo oscuro
-        document.body.classList.remove('dark-mode');
-    }
-});
+if (modoToggle) {
+    modoToggle.addEventListener('change', function() {
+        if (modoToggle.checked) {
+            // Activar el modo oscuro
+            document.body.classList.add('dark-mode');
+        } else {
+            // Desactivar el modo oscuro
+            document.body.classList.remove('dark-mode');
+        }
+    });
+}
